674aa13cfba47bc36a9f1ebb82fee20f2bef9f21
mrchtr <matthias.r1092@gmail.com>
2023-06-19 15:00:44 +0200

    Adapt project to new account setup

--- training_pipeline/images/lambda/build_and_push.sh
+++ training_pipeline/images/lambda/build_and_push.sh
@@ -8,8 +8,8 @@ echo "Profile-Name: operations"
 docker build -t lambda-image -f images/lambda/Dockerfile --platform linux/amd64 .
 
 # Login to docker registry
-aws ecr get-login-password --region eu-central-1 --profile operations | docker login --username AWS --password-stdin $operations.dkr.ecr.eu-central-1.amazonaws.com
+aws ecr get-login-password --region eu-west-3 --profile operations | docker login --username AWS --password-stdin $operations.dkr.ecr.eu-west-3.amazonaws.com
 
 # Tag and Push Docker Image to Container Registry
-docker tag lambda-image:latest $operations.dkr.ecr.eu-central-1.amazonaws.com/lambda-image:latest
-docker push $operations.dkr.ecr.eu-central-1.amazonaws.com/lambda-image:latest
+docker tag lambda-image:latest $operations.dkr.ecr.eu-west-3.amazonaws.com/lambda-image:latest
+docker push $operations.dkr.ecr.eu-west-3.amazonaws.com/lambda-image:latest
--- training_pipeline/images/train/build_and_push.sh
+++ training_pipeline/images/train/build_and_push.sh
@@ -5,14 +5,14 @@ echo "Profile-Id: $operations"
 echo "Profile-Name: operations"
 
 # Login to AWS docker registry
-aws ecr get-login-password --region eu-central-1 --profile operations | docker login --username AWS --password-stdin 763104351884.dkr.ecr.eu-central-1.amazonaws.com
+aws ecr get-login-password --region eu-west-3 --profile operations | docker login --username AWS --password-stdin 763104351884.dkr.ecr.eu-west-3.amazonaws.com
 
 # build docker image (if running on M1/M2 --> specify platform with : --platform linux/amd64)
 docker build -t training-image -f images/train/Dockerfile --platform linux/amd64 .
 
 # Login to docker registry
-aws ecr get-login-password --region eu-central-1--profile operations | docker login --username AWS --password-stdin $operations.dkr.ecr.eu-central-1.amazonaws.com
+aws ecr get-login-password --region eu-west-3 --profile operations | docker login --username AWS --password-stdin $operations.dkr.ecr.eu-west-3.amazonaws.com
 
 # Tag and Push Docker Image to Container Registry
-docker tag training-image:latest $operations.dkr.ecr.eu-central-1.amazonaws.com/training-image:latest
-docker push $operations.dkr.ecr.eu-central-1.amazonaws.com/training-image:latest
+docker tag training-image:latest $operations.dkr.ecr.eu-west-3.amazonaws.com/training-image:latest
+docker push $operations.dkr.ecr.eu-west-3.amazonaws.com/training-image:latest
